f048f0672603e61e75b269976a625854492d92d4
pranavgugale561-prog <pranavgugale561@gmail.com>
2026-08-23 00:59:56 +0530

    chore: test firebaseAdmin import

--- api/test.js
+++ api/test.js
@@ -1,2 +1,2 @@
-import { getApps } from 'firebase-admin/app';
-export default function handler(req, res) { res.status(200).json({ ok: true, apps: getApps().length }); }
+import { verifyToken } from './_lib/firebaseAdmin.js';
+export default function handler(req, res) { res.status(200).json({ ok: true }); }
