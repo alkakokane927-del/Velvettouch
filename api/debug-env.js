30fed40138193f53a1d0fc712b17f39a58f60d70
pranavgugale561-prog <pranavgugale561@gmail.com>
2026-08-22 23:05:04 +0530

    chore: update debug env to check cloudinary

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     hasTursoUrl: !!process.env.TURSO_DATABASE_URL,
-    tursoUrlStartsRight: process.env.TURSO_DATABASE_URL ? process.env.TURSO_DATABASE_URL.startsWith("libsql://") : false,
-    tursoUrlHasQuotes: process.env.TURSO_DATABASE_URL ? process.env.TURSO_DATABASE_URL.includes("\"") : false,
     hasTursoToken: !!process.env.TURSO_AUTH_TOKEN,
-    hasFirebaseKey: !!process.env.FIREBASE_PRIVATE_KEY
+    hasFirebaseKey: !!process.env.FIREBASE_PRIVATE_KEY,
+    hasCloudinary: !!process.env.CLOUDINARY_CLOUD_NAME
   });
 }
